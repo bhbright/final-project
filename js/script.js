da20def9081991e76f1977113cfd8311c9f96962
bhbright <bhstormigeometrydash@gmail.com>
2026-02-03 02:22:54 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+// Select the hamburger button and the nav menu
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-navToggle.addEventListener('click', () => {
+// Add click event listener
+navToggle.addEventListener('click', function() {
+  // Toggle the 'active' class
   navLinks.classList.toggle('active');
 });
